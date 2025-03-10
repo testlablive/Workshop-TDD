b249c0a11eae7c78aa49314f7fb3c30b427218b9
Joanna Stenzel <Jst@objektkultur.de>
2025-03-10 15:54:12 +0100

    delete SessionContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { FluentProvider, webLightTheme } from '@fluentui/react-components';
-import { SessionProvider } from './context/SessionContext';
-import { HomePage } from './pages/HomePage';
-import { SessionPage } from './pages/SessionPage';
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+import { HomePage } from "./pages/HomePage";
+import { SessionPage } from "./pages/SessionPage";
 
 function App() {
   return (
     <FluentProvider theme={webLightTheme}>
-      <SessionProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/session/:sessionId" element={<SessionPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </BrowserRouter>
-      </SessionProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/session/:sessionId" element={<SessionPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </FluentProvider>
   );
 }
