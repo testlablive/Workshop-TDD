d0a3eb49886d91e25a7c50d42d609663c40e81e7
Joanna Stenzel <Jst@objektkultur.de>
2025-03-10 16:28:46 +0100

    init playwright

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    port: 3000,
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
