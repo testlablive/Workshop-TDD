b6170032bd117137bfd6842c0ee6f3f631249b57
Jonathan Waldenberger <jwa@objektkultur.de>
2025-03-18 15:28:26 +0100

    1-GREEN: Implement minimal controller to return Created Status

---
+++ backend/PlanningPokerApi/SessionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlanningPokerApi;
+
+[ApiController]
+[Route("[controller]")]
+public class SessionController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult StartSession()
+    {
+        return Created("", null);
+    }
+}
+
