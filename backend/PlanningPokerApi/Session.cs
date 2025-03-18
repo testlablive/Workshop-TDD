ca61dc42c2da7f702324ddc1cd886edf02084a8d
Jonathan Waldenberger <jwa@objektkultur.de>
2025-03-18 18:21:21 +0100

    4-GREEN: hardcode session details in constructor

--- backend/PlanningPokerApi/Session.cs
+++ backend/PlanningPokerApi/Session.cs
@@ -3,10 +3,16 @@
     public class Session
     {
         public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
 
         public static Session StartSession()
         {
-            return new Session { Id = Guid.NewGuid() };
+            return new Session {
+                Id = Guid.NewGuid(),
+                Title = "Any valid planning session",
+                Description = "Planning Poker for any valid planning session"
+            };
         }
     }
 }
