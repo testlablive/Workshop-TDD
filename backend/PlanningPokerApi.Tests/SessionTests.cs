d9fd122615dfb1b18a062c6f0c7d0bb5af2f665b
Jonathan Waldenberger <jwa@objektkultur.de>
2025-03-18 15:07:57 +0100

    1-RED: Initial test for starting a new planning poker session. For now just check for a 201 Created response.

---
+++ backend/PlanningPokerApi.Tests/SessionTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+
+namespace PlanningPokerApi.Tests;
+
+public class SessionTests
+{
+    [Fact]
+    public async Task StartSession_WithValidRequest_ReturnsCreated()
+    {
+        var webapp = new WebApplicationFactory<Program>();
+        var client = webapp.CreateClient();
+        var addResponse = await client.PostAsJsonAsync("session", new
+        {
+            title = "Any valid planning session",
+            description = "Planning Poker for any valid planning session"
+        });
+
+        Assert.Equal(System.Net.HttpStatusCode.Created, addResponse.StatusCode);
+    }
+}
